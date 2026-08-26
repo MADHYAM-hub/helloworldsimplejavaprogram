909bfac48f86dcc5a5c5ef2ef7d2a2fbbad36165
MADHYAM-hub <vikasmishra943@gmail.com>
2026-08-26 19:42:25 +0530

    Add interactive user input

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,15 @@
-public class HelloWorld {public static void main(String[] args) {
-    System.out.println("Hello, World!");
-}
-    
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is your name? ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name + "!");
+
+        scanner.close();
+    }
 }
