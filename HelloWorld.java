9ad78bd1b0c86c0c7f7557b3068eb84810c805f9
MADHYAM-hub <vikasmishra943@gmail.com>
2026-08-30 12:40:52 +0530

    Add name and age input

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,7 +8,11 @@ public class HelloWorld {
         System.out.print("What is your name? ");
         String name = scanner.nextLine();
 
+        System.out.print("How old are you? ");
+        int age = scanner.nextInt();
+
         System.out.println("Hello, " + name + "!");
+        System.out.println("You are " + age + " years old.");
 
         scanner.close();
     }
